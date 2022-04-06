e82bec0eeebefe2ce8b9c2c8dece097ec4819798
vaishnavi0393 <vbhoye90@gmail.com>
2022-04-06 18:55:47 +0530

    csp added successfully

--- kyc/static/kyc/js/show_client_data.js
+++ kyc/static/kyc/js/show_client_data.js
@@ -19,11 +19,20 @@ $(window).on('load',function(){
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
         cell1.innerHTML = dt[j.toString()]['pk'];
-        cell2.innerHTML = "<button type='button' onclick='myfunction(this.id)' class='btn btn-link' id='"+j+"' data-bs-toggle='modal' data-bs-target='#myModal'>"+ dt[j.toString()]['fields']['name']+"</button>";
+        cell2.innerHTML = "<button type='button' class='btn btn-link' name='g' id='"+j+"' data-bs-toggle='modal' data-bs-target='#myModal'>"+ dt[j.toString()]['fields']['name']+"</button>";
         cell3.innerHTML = dt[j.toString()]['fields']['contact_no'];
-        cell4.innerHTML = dt[j.toString()]['fields']['email_ad'];
-        
+        cell4.innerHTML = dt[j.toString()]['fields']['email_ad'];      
       }
+      $("button[name=g]").on("click",function(){
+        var id = $(this).attr('id');
+        var keys = Object.keys(dt[id.toString()]['fields']);
+        var val = Object.values(dt[id.toString()]['fields']);
+        var p,v,txt="";
+        for(p=0,v=0;p<keys.length,v<val.length;p++,v++){
+            txt += "<tr><td>"+ keys[p].toUpperCase()+"</td><td>" + val[v] +"</td></tr>";
+        }
+        document.getElementById("det").innerHTML = txt;
+      });
     }
   }
 
@@ -35,12 +44,8 @@ $(window).on('load',function(){
 });
 
 
-function myfunction(id){
-  var keys = Object.keys(dt[id.toString()]['fields']);
-  var val = Object.values(dt[id.toString()]['fields']);
-  var p,v,txt="";
-  for(p=0,v=0;p<keys.length,v<val.length;p++,v++){
-      txt += "<tr><td>"+ keys[p].toUpperCase()+"</td><td>" + val[v] +"</td></tr>";
-  }
-  document.getElementById("det").innerHTML = txt;
-}  
+
+
+
+  
+
